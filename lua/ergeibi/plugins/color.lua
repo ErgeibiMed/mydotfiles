eeaee823b0c2376ea11c69fc83a2e350ab81fcec
ErgeibiMed <ergeibi.mohamed@gmail.com>
2024-12-01 17:53:00 +0100

    changing the color plugin

--- lua/ergeibi/plugins/color.lua
+++ lua/ergeibi/plugins/color.lua
@@ -1,74 +1,85 @@
 return {
-    "EdenEast/nightfox.nvim",  -- lazy
-      config = function()
-                -- Default options
-        require('nightfox').setup({
-            options = {
-                -- Compiled file's destination location
-                compile_path = vim.fn.stdpath("cache") .. "/nightfox",
-                compile_file_suffix = "_compiled", -- Compiled file suffix
-                transparent = false,     -- Disable setting background
-                terminal_colors = true,  -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-                dim_inactive = true,    -- Non focused panes set to alternative background
-                module_default = true,   -- Default enable value for modules
-                colorblind = {
-                    enable = false,        -- Enable colorblind support
-                    simulate_only = false, -- Only show simulated colorblind colors and not diff shifted
-                    severity = {
-                        protan = 0,          -- Severity [0,1] for protan (red)
-                        deutan = 0,          -- Severity [0,1] for deutan (green)
-                        tritan = 1,          -- Severity [0,1] for tritan (blue)
-                    },
-                },
-                styles = {               -- Style to be applied to different syntax groups
-                    comments = "NONE",     -- Value is any valid attr-list value `:help attr-list`
-                    conditionals = "NONE",
-                    constants = "NONE",
-                    functions = "NONE",
-                    keywords = "NONE",
-                    numbers = "NONE",
-                    operators = "NONE",
-                    strings = "NONE",
-                    types = "NONE",
-                    variables = "NONE",
-                },
-                inverse = {             -- Inverse highlight for different types
-                    match_paren = false,
-                    visual = false,
-                    search = false,
-                },
-                modules = {             -- List of various plugins and additional options
-                    -- ...
-                },
-            },
-            palettes = {
-                -- Custom duskfox with black background
-                nightfox = {
-                    bg1 = "#000000", -- Black background
-                    bg0 = "#1d1d2b", -- Alt backgrounds (floats, statusline, ...)
-                    bg3 = "#121829", -- 55% darkened from stock
-                    sel0 = "#141b24", -- 55% darkened from stock
-                },
-            },
-            specs = {
-                all = {
-                    inactive = "bg0", -- Default value for other styles
-                },
-                nightfox = {
-                    inactive = "#090909", -- Slightly lighter then black background
-                },
-            },
-            groups = {
-                all = {
-                    NormalNC = { fg = "fg0", bg = "inactive" }, -- Non-current windows
-                },
-            },
-            --palettes = {},
-            --specs = {},
-            --groups = {},
-        })
+    "rose-pine/neovim",
+    config=function ()
 
-        -- setup must be called before loading
-        vim.cmd("colorscheme nightfox")
+    require("rose-pine").setup({
+        variant = "main", -- auto, main, moon, or dawn
+        dark_variant = "moon", -- main, moon, or dawn
+        dim_inactive_windows = false,
+        extend_background_behind_borders = true,
+
+        enable = {
+            terminal = true,
+            legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+            migrations = true, -- Handle deprecated options automatically
+        },
+
+        styles = {
+            bold = true,
+            italic = true,
+            transparency = false,
+        },
+
+        groups = {
+            border = "muted",
+            link = "iris",
+            panel = "surface",
+
+            error = "love",
+            hint = "iris",
+            info = "foam",
+            note = "pine",
+            todo = "rose",
+            warn = "gold",
+
+            git_add = "foam",
+            git_change = "rose",
+            git_delete = "love",
+            git_dirty = "rose",
+            git_ignore = "muted",
+            git_merge = "iris",
+            git_rename = "pine",
+            git_stage = "iris",
+            git_text = "rose",
+            git_untracked = "subtle",
+
+            h1 = "iris",
+            h2 = "foam",
+            h3 = "rose",
+            h4 = "gold",
+            h5 = "pine",
+            h6 = "foam",
+        },
+
+        palette = {
+            -- Override the builtin palette per variant
+            -- moon = {
+            --     base = '#18191a',
+            --     overlay = '#363738',
+            -- },
+        },
+
+        highlight_groups = {
+            -- Comment = { fg = "foam" },
+            -- VertSplit = { fg = "muted", bg = "muted" },
+        },
+
+        --before_highlight = function(group, highlight, palette)
+            -- Disable all undercurls
+            -- if highlight.undercurl then
+            --     highlight.undercurl = false
+            -- end
+            --
+            -- Change palette colour
+            -- if highlight.fg == palette.pine then
+            --     highlight.fg = palette.foam
+            -- end
+        --end,
+    })
+
+    vim.cmd("colorscheme rose-pine")
+    -- vim.cmd("colorscheme rose-pine-main")
+    -- vim.cmd("colorscheme rose-pine-moon")
+    -- vim.cmd("colorscheme rose-pine-dawn")
     end
 }
