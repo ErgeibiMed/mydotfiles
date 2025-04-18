a6d7525d1d3c50df829be5b7fe4fcfe97b2b8971
ErgeibiMed <ergeibi.mohamed@gmail.com>
2025-04-18 01:11:24 +0000

    auto-format on save

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -6,6 +6,16 @@ require("config.lazy_init")
 vim.diagnostic.config({virtual_text = { current_line = true }})
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+    callback = function()
+        local mode = vim.api.nvim_get_mode().mode
+        local filetype = vim.bo.filetype
+        if vim.bo.modified == true and mode == 'n' and filetype ~= "oil" then
+            vim.cmd('lua vim.lsp.buf.format()')
+        else
+        end
+    end
+})
 
 local augroup = vim.api.nvim_create_augroup
 local TheMoGroup = augroup('TheMo', {})
