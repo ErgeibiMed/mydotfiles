bbec070c17d5087432ddddd70d1d6e3225dd0e70
ErgeibiMed <ergeibi.mohamed@gmail.com>
2025-07-30 18:20:26 +0000

    removed lsps bcause they gave headeacks for days from this moment forward i shall not use an lsp and that a promise to myself as i enbark on a journey to be a real software developper (whatever that means)

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@ require("config")
 
 
 vim.cmd(":hi statusline guibg=NONE")
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 
 vim.api.nvim_set_option("clipboard", "unnamedplus")
 vim.lsp.set_log_level('off')
+
--- lua/config/init.lua
+++ lua/config/init.lua
@@ -3,28 +3,17 @@ require("config.remap")
 require("config.lazy_init")
 
 
---vim.diagnostic.config(
---    {
---        virtual_text =
---        { current_line = true }
---    }
---
---)
+vim.diagnostic.config(
+    {
+        virtual_text =
+        { current_line = true }
+    }
+
+)
 
 
 vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>")
 
-
---vim.api.nvim_create_autocmd("BufWritePre", {
---    callback = function()
---        local mode = vim.api.nvim_get_mode().mode
---        local filetype = vim.bo.filetype
---        if vim.bo.modified == true and mode == 'n' and filetype ~= "oil" then
---            vim.cmd('lua vim.lsp.buf.format()')
---        else
---        end
---    end
---})
 
 local augroup = vim.api.nvim_create_augroup
 local TheMoGroup = augroup('TheMo', {})
@@ -49,34 +38,37 @@ autocmd({ "BufWritePre" }, {
     command = [[%s/\s\+$//e]],
 })
 
---
---  default ones fron neovim
---    grn in Normal mode maps to vim.lsp.buf.rename()
---    grr in Normal mode maps to vim.lsp.buf.references()
---    gri in Normal mode maps to vim.lsp.buf.implementation()
---    gO in Normal mode maps to vim.lsp.buf.document_symbol() (this is analogous to the gO mappings in help buffers and :Man page buffers to show a “table of contents”)
---    gra in Normal and Visual mode maps to vim.lsp.buf.code_action()
---    CTRL-S in Insert and Select mode maps to vim.lsp.buf.signature_help()
---    [d and ]d move between diagnostics in the current buffer ([D jumps to the first diagnostic, ]D jumps to the last)
---
---  mine
---
-autocmd('LspAttach', {
-    group = TheMoGroup,
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "gdw", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-        --vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        --vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "gsh", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "gdn", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "gdp", function() vim.diagnostic.goto_prev() end, opts)
-    end
-})
+
+                         --  default ones fron neovim
+                         --    grn in Normal mode maps to vim.lsp.buf.rename()
+                         --    grr in Normal mode maps to vim.lsp.buf.references()
+                         --    gri in Normal mode maps to vim.lsp.buf.implementation()
+                         --    gO in Normal mode maps to vim.lsp.buf.document_symbol()
+--                                (this is analogous to the gO mappings in help buffers
+--                                and :Man page buffers to show a “table of contents”)
+                         --    gra in Normal and Visual mode maps to vim.lsp.buf.code_action()
+                         --    CTRL-S in Insert and Select mode maps to vim.lsp.buf.signature_help()
+                         --    [d and ]d move between diagnostics in the current buffer
+--                        --([D jumps to the first diagnostic, ]D jumps to the last)
+                         --------------------------------
+                         --  mine
+--------------------------------
+                    ----autocmd('LspAttach', {
+                    ----    group = TheMoGroup,
+                    ----    callback = function(e)
+                    ----        local opts = { buffer = e.buf }
+                    ----        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                    ----        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                    ----        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+                    ----        vim.keymap.set("n", "gdw", function() vim.diagnostic.open_float() end, opts)
+                    ----        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+                    ----        --vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+                    ----        --vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+                    ----        vim.keymap.set("i", "gsh", function() vim.lsp.buf.signature_help() end, opts)
+                    ----        vim.keymap.set("n", "gdn", function() vim.diagnostic.goto_next() end, opts)
+                    ----        vim.keymap.set("n", "gdp", function() vim.diagnostic.goto_prev() end, opts)
+                    ----    end
+                    ----})
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
